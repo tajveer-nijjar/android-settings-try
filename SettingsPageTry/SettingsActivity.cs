d55bb09c919abeab4490e5122f9062be199c974c
Tajveer Nijjar <Tajveer.Nijjar@connexionz.com>
2020-12-04 10:40:09 +1300

    Wroking prototype.

---
+++ SettingsPageTry/SettingsActivity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Preferences;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Support.V7.Preferences;
+using Android.Views;
+using Android.Widget;
+
+namespace SettingsPageTry
+{
+    [Activity(Label = "SettingsActivity")]
+    public class SettingsActivity : PreferenceFragmentCompat
+    {
+        public override void OnCreatePreferences(Bundle savedInstanceState, string rootKey)
+        {
+            SetPreferencesFromResource(Resource.Xml.preferences, rootKey);
+        }
+
+        //protected override void OnCreate(Bundle savedInstanceState)
+        //{
+        //    base.OnCreate(savedInstanceState);
+
+        //    SetPreferencesFromResource(Resource.Xml.preferences)
+
+        //    // Create your application here
+        //    if(FragmentManager.FindFragmentById(Resource.Id.content) == null)
+        //    {
+        //        FragmentManager.BeginTransaction().Add(Resource.Id.content, SettingsFragment).Commit();
+        //    }
+        //}
+    }
+}
